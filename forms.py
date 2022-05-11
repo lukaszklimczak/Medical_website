9a5c809085598d17eb09451200fd41d990517d7d
lukaszklimczak <lukaszklimczak86@gmail.com>
2022-05-11 23:38:16 +0200

    Login features added

---
+++ forms.py
@@ -0,0 +1,25 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired
+from flask_ckeditor import CKEditorField
+
+
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Hasło", validators=[DataRequired()])
+    first_name = StringField("Imię", validators=[DataRequired()])
+    last_name = StringField("Nazwisko", validators=[DataRequired()])
+    mobile = StringField("Telefon", validators=[DataRequired()])
+    submit = SubmitField("Zarejestruj")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Zaloguj")
+
+
+class CreatePostForm(FlaskForm):
+    title = StringField("Tytuł Posta", validators=[DataRequired()])
+    body = CKEditorField("Treść posta", validators=[DataRequired()])
+    submit = SubmitField("Opublikuj")
