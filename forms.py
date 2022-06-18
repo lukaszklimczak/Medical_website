1e51e1d44de4909cb182a599c25982ab07606039
lukaszklimczak <lukaszklimczak86@gmail.com>
2022-06-18 22:55:08 +0200

    First part of booking features added

--- forms.py
+++ forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
+from datetime import datetime, date
+from wtforms.fields.html5 import DateTimeField, DateField, TimeField
 
 
 class RegisterForm(FlaskForm):
@@ -24,3 +26,11 @@ class CreatePostForm(FlaskForm):
     body = CKEditorField("Treść posta", validators=[DataRequired()])
     image_url = StringField("Link do zdjęcia", validators=[DataRequired(), URL()])
     submit = SubmitField("Opublikuj")
+
+
+class BookVisitForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    date = DateField("Data wizyty", format='%Y-%m-%d', validators=[DataRequired()])
+    starts_at = TimeField("Godzina rozpoczęcia wizyty", format='%H:%M', validators=[DataRequired()])
+    # ends_at = DateTimeField("Godzina zakończenia wizyty", format='%H:%M:%S')
+    submit = SubmitField("Dodaj wizytę")
